2721b99dd743f912db62dfefc1d86abdd3837643
0FAUXS <lumin.lumin@du.edu>
2024-10-30 16:33:42 +0000

    numberCruncher def

---
+++ src/supernormal/numberCruncher.py
@@ -0,0 +1,14 @@
+"""A module that processes numbers"""
+def crunch(x,y):
+    """A function to do fancy math
+
+    Parameters
+    ----------
+        x : int
+            add first
+        y : int
+            add second
+    Returns
+        summation
+    """
+    return x + y
